35b9c204bbc228dbc112f256ebe2b5d55546f407
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2025-09-06 08:57:58 +0530

    Update Playwright config and enhance test script
    Switched to double quotes in playwright.config.js and commented out Firefox and WebKit projects. Improved test_1.spec.js by adding page title and URL validation, console logs, and usage instructions.

--- tests/test_1.spec.js
+++ tests/test_1.spec.js
@@ -1,6 +1,20 @@
-const { test, expect } = require("@playwright/test");
-const { assert } = require("console");
+const { test, expect } = require("@playwright/test"); //async and await is used to maintain the promise as JS is async language
 
 test("Home Page", async ({ page }) => {
   await page.goto("https://demoblaze.com/");
+
+  //to display and validate page title
+  var pageTitle = page.title();
+  console.log("Page Title is", pageTitle);
+  await expect(page).toHaveTitle("STORE");
+
+  //to display and validate page URL
+  var pageURL = page.url();
+  console.log("Page URL is", pageURL);
+  expect(pageURL).toContain("demoblaze");
+  page.close();
 });
+
+//commands
+//1. To Run all Test : npx playwright test
+//2. To Run Specific Test : npx playwright test {your_test_name}
