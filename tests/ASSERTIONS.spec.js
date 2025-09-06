ea115a59ca3cb92a29452667962c52185ba65d08
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2025-09-06 14:06:24 +0530

    Add initial Playwright test for Google homepage title assertion

---
+++ tests/ASSERTIONS.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  
+    await page.goto("https://www.google.com/");
+    
+
+
+
+  await page.close();
+});
