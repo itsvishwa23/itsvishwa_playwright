a63d0a67581b57526e8dc7c6fcb2d0246cd0a1f4
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2025-09-06 17:35:48 +0530

    Add soft assertions test for demoblaze homepage

--- tests/ASSERTIONS.spec.js
+++ tests/ASSERTIONS.spec.js
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
+import { assert } from "console";
 
 test("Assertions", async ({ page }) => {
   
     await page.goto("https://demoblaze.com/index.html");
     const login='//a[@id="login2"]';
+    const txt='//a[@id="cat"]';
 
     await expect(page.locator(login)).toBeVisible();
-
+    await expect(page.locator(txt)).toContainText('CATEGORIES');
     await page.close();
 });
---
+++ tests/Soft_Assertions.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+import { assert } from "console";
+
+test("Soft Assertions", async ({ page }) => {
+  
+    await page.goto("https://demoblaze.com/index.html");
+    const cat_div='//a[@id="cat"]';
+
+    
+    await expect.soft(page).toHaveTitle('Vishwanath')
+    await expect(page).toHaveURL('https://demoblaze.com/index.html')
+    await expect(page.locator(cat_div)).toBeVisible();
+    await page.close();
+});
