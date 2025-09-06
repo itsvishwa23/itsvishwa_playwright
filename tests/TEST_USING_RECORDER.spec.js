53d4cff7172797d99b749cab7fa10616875893e8
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2025-09-06 14:30:34 +0530

    Rename test for clarity

--- tests/TEST_USING_RECORDER.spec.js
+++ tests/TEST_USING_RECORDER.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Test Using Recorder', async ({ page }) => {
   await page.goto('https://demoblaze.com/');
   await page.getByRole('link', { name: 'Log in' }).click();
   await page.locator('#loginusername').click();
